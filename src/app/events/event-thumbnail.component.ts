563dc3ba748f8f4b67f39f5a266d8c5990a1ed8a
unknown <8574aman@gmail.com>
2020-11-12 19:47:45 +0530

    used ngif to handle null data and ngSwitch to choose style based on data , used ngStyle

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,17 +4,24 @@ import {Component,Input, Output , EventEmitter} from '@angular/core'
     selector  : 'event-thumbnail',
     template : `    <div class= "well hoverwell thumbnail">
     <h2>{{event.name}}</h2>
-   <div>Date : {{event.date}}</div>
-   <div>Time : {{event.time}}</div>
-   <div>Price : {{event.price}}</div>
-   <div>
-   <span>Location : {{event.location.address}}</span>
-   <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+   <div>Date : {{event?.date}}</div>
+   <div [ngClass] = "getStartTimeClass()" [ngSwitch] = "event?.time">
+   Time : {{event?.time}}
+   <span *ngSwitchCase="'8:00 am'">Early start</span>
+   <span *ngSwitchCase="'10:00 am'">late start</span>
+   <span *ngSwitchDefault>Normal start</span>
+   
+   </div>
+   <div [ngStyle] = "getPriceStyle()">Price : {{event?.price}}</div>
+   <div *ngIf="event?.location">
+   <span>Location : {{event?.location.address}}</span>
+   <span class="pad-left">{{event?.location.city}}, {{event?.location.country}}</span>
    </div>
 </div>`
 ,
 
 styles: [`
+  
     .thumbnail {min-height : 210px;}
     .pad-left  {margin-left : 10px;}
 `]
@@ -25,8 +32,21 @@ styles: [`
 export class EventThumbnailComponent{
 @Input() event :any
 
-public logfoo(){
-    console.log('foo');
+getStartTimeClass(){
+   // const isEarlyStart = this.event && this.event.time === '8:00 am'
+    //return {green : isEarlyStart, bold : isEarlyStart}
+
+    if(this.event != null && this.event.time === '8:00 am')
+    return ['green','bold']
+
+    return ''
+}
+
+getPriceStyle():any{
+    if(this.event && this.event.price>800)
+    return {color:'red', 'font-weight' : 'bold'}
+
+    return{}
 }
 }
 
