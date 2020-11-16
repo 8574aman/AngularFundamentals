3d1a2cf7b606981764ba445676b899c463a4d702
unknown <8574aman@gmail.com>
2020-11-16 23:27:47 +0530

    used ActivatedRouteService to render paramter from the URL

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, Injectable } from '@angular/core';
 import { EventService } from '../shared/event.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
 templateUrl : './event-details.component.html',
@@ -13,9 +14,9 @@ styles : [
 @Injectable()
 export class EventDetailsComponent{
     event : any
-    constructor(private eventService:EventService){}
+    constructor(private eventService:EventService , private route : ActivatedRoute){}
     ngOnInit(){
-        console.log("Event details called")
-         this.event = this.eventService.getEvent(1)
+
+         this.event = this.eventService.getEvent(+this.route.snapshot.params['id'])
     }
 }
--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -96,7 +96,7 @@ const  EVENTS = [
       date: '4/15/2037',
       time: '9:00 am',
       price: 950.00,
-      imageUrl: '/app/assets/images/ng-nl.png',
+      imageUrl: '/assets/images/ng-nl.png',
       
       sessions: [
         {
@@ -152,7 +152,7 @@ const  EVENTS = [
       date: '5/4/2037',
       time: '9:00 am',
       price: 759.00,
-      imageUrl: '/app/assets/images/ng-conf.png',
+      imageUrl: '/assets/images/ng-conf.png',
       location: {
         address: 'The Palatial America Hotel',
         city: 'Salt Lake City',
@@ -234,7 +234,7 @@ const  EVENTS = [
       date: '6/10/2037',
       time: '8:00 am',
       price: 800.00,
-      imageUrl: '/app/assets/images/basic-shield.png',
+      imageUrl: '/assets/images/basic-shield.png',
       location: {
         address: 'The UN Angular Center',
         city: 'New York',
@@ -283,7 +283,7 @@ const  EVENTS = [
       date: '2/10/2037',
       time: '9:00 am',
       price: 400.00,
-      imageUrl: '/app/assets/images/ng-vegas.png',
+      imageUrl: '/assets/images/ng-vegas.png',
       location: {
         address: 'The Excalibur',
         city: 'Las Vegas',
